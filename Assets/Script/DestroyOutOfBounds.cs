495feb555e5d11a229137c2d13be015b3ad54f08
CorneliusTheDestroyer <cornestols7@gmail.com>
2020-03-26 22:21:59 +0200

    Lesson 2.4

--- Assets/Script/DestroyOutOfBounds.cs
+++ Assets/Script/DestroyOutOfBounds.cs
@@ -9,7 +9,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,6 +21,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             //Destroy(gameObject);
         }
     }
