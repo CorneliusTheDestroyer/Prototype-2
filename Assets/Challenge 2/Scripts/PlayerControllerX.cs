6096da7c18642539471309aaf6f9cc6dda4d394e
CorneliusTheDestroyer <cornestols7@gmail.com>
2020-03-27 08:57:30 +0200

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(mainCoroutine());
+        }
+    }
+
+    IEnumerator mainCoroutine()
+    {
+        Debug.Log("Started at: " + Time.time);
+
+        yield return new WaitForSeconds(5);
+
+        Debug.Log("Finished at: " + Time.time);
+
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
